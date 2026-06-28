4f32cf813ae2637db791d3271eb99f7fc1abd8c5
mahady13 <mohiuddinmahady@gmail.com>
2026-06-28 08:10:11 +0600

    added deeplearning model

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ import torch
 import torch.nn as nn
 import pandas as pd
 
+df=pd.read_csv("riceClassification.csv")
+df.drop(['id'],axis=1,inplace=True)
+
+class MyModel(nn.Module):
+    def __init__(self, input_dim=10):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 16),
+            nn.ReLU(),
+            nn.Linear(16, 1)
+        )
+
+    def forward(self, X):
+        return self.net(X)
